adbd838bae4e168e0c2c5403b3f4309606f5f423
FunCult <84736649+FunCult@users.noreply.github.com>
2021-08-18 16:26:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -82,4 +82,4 @@ function getCurrentLocation(event) {
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-searchCity("");
+search("");
