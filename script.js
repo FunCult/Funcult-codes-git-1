f1dc1765204435e17767dbdfc4809a9ccb015607
FunCult <84736649+FunCult@users.noreply.github.com>
2021-08-30 19:13:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -151,8 +151,8 @@ search("Santa Rosa");
 
 function showWeather(response) {
   let h1 = document.querySelector("h1");
-  let temperature = Math.round(response.data.main.temp);
-  h1.innerHTML = `It is currently ${date}° in ${response.data.name}`;
+
+  h1.innerHTML = `It is currently ${day}° in `;
 }
 
 function retrievePosition(position) {
