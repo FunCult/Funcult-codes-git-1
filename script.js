5cded0e48489a186af16b4ed1e203108ac3ce47a
FunCult <84736649+FunCult@users.noreply.github.com>
2021-08-31 19:45:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function formatDate(timestamp) {
   ];
 
   let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes} 🕙 ◕_◕ ༽つ`;
+  return `${day} ${hours}:${minutes} 🕙(✿◠‿◠) `;
 }
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
